fdd7feb3dbf7b4aa354e43bafa1d4e44480ddfe4
Johannes Nikolai <186506@stud.hvl.no>
2026-04-15 14:53:02 +0200

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentSearch;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ models/Course.cs
@@ -0,0 +1,5 @@
+﻿namespace StudentSearch.models;
+
+public class Course {
+    
+}
